b90ad36a67538ec140646ace94ca5dc7b101ab44
lucassilvajs <lucasjesusssilva@gmail.com>
2019-11-17 21:37:24 -0300

    Configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "58835ac5e39936",
+      pass: "062280a4199626"
+    },
+    secure: false,
+    default: {
+        from: 'Support <noreply@gobarber.com>'
+    }
+};
